520d8c5fa1a4be49fecf72c11f6cbc762df1bb8f
pritbhavsar8 <143060884+pritbhavsar8@users.noreply.github.com>
2024-04-01 10:14:45 +0530

    Initial commit

---
+++ android/app/src/main/java/com/flexion/doctor_hunt/doctor_hunt/MainActivity.java
@@ -0,0 +1,6 @@
+package com.flexion.doctor_hunt.doctor_hunt;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/View/Screen/Splash_Screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width,
+            child: Image.asset("img/Splash screen.jpg"),
+          )
+        ],
+      ),
+
+    );
+  }
+}
